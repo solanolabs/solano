5d73e3945e632238643009dd955c3471852c17eb
Andrii Lazarchuk <alazarchuk@solanolabs.com>
2016-08-15 16:17:53 +0000

    Add prototype that adds `org` option to `config` command

--- lib/solano/cli/commands/config.rb
+++ lib/solano/cli/commands/config.rb
@@ -6,6 +6,7 @@ module Solano
     The scope argument can be 'suite', 'repo', 'org' (if you are a member of
     one organization), or 'org:an_organization_name' (if you are a member of
     multiple organizations). The default is 'suite'."
+    method_option :org, type: :string
     def config(scope="suite")
       params = {:repo => true}
       if scope == 'suite' then
@@ -13,8 +14,11 @@ module Solano
       end
       solano_setup(params)
 
+      parsed_options = {}
+      parsed_options['account'] = options[:org] if !options[:org].nil?
+
       begin
-        config_details = @solano_api.get_config_key(scope)
+        config_details = @solano_api.get_config_key(scope, nil, parsed_options)
         show_config_details(scope, config_details['env'])
       rescue TddiumClient::Error::API => e
         exit_failure Text::Error::LIST_CONFIG_ERROR
@@ -27,6 +31,7 @@ module Solano
     The scope argument can be 'suite', 'repo', 'org' (if you are a member of
     one organization), or 'org:an_organization_name' (if you are a member of
     multiple organizations)."
+    method_option :org, type: :string
     define_method "config:add" do |scope, key, value|
       params = {:repo => true}
       if scope == 'suite' then
@@ -34,9 +39,12 @@ module Solano
       end
       solano_setup(params)
 
+      parsed_options = {}
+      parsed_options['account'] = options[:org] if !options[:org].nil?
+
       begin
         say Text::Process::ADD_CONFIG % [key, value, scope]
-        result = @solano_api.set_config_key(scope, key, value)
+        result = @solano_api.set_config_key(scope, key, value, parsed_options)
         say Text::Process::ADD_CONFIG_DONE % [key, value, scope]
       rescue TddiumClient::Error::API => e
         exit_failure Text::Error::ADD_CONFIG_ERROR
@@ -46,6 +54,7 @@ module Solano
     end
 
     desc "config:remove [SCOPE] [KEY]", "Remove config variable NAME from SCOPE."
+    method_option :org, type: :string
     define_method "config:remove" do |scope, key|
       params = {:repo => true}
       if scope == 'suite' then
@@ -53,9 +62,12 @@ module Solano
       end
       solano_setup(params)
 
+      parsed_options = {}
+      parsed_options['account'] = options[:org] if !options[:org].nil?
+
       begin
         say Text::Process::REMOVE_CONFIG % [key, scope]
-        result = @solano_api.delete_config_key(scope, key)
+        result = @solano_api.delete_config_key(scope, key, parsed_options)
         say Text::Process::REMOVE_CONFIG_DONE % [key, scope]
       rescue TddiumClient::Error::API => e
         exit_failure Text::Error::REMOVE_CONFIG_ERROR
--- lib/solano/version.rb
+++ lib/solano/version.rb
@@ -1,5 +1,5 @@
 # Copyright (c) 2011-2016 Solano Labs All Rights Reserved
 
 module Solano
-  VERSION = "1.28.7"
+  VERSION = "1.28.8"
 end
